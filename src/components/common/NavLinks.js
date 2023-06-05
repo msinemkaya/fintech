209665de17bb5e4f4e3658c1aebbb2eb95f9dc6f
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-06-05 23:40:38 +0300

    feat: added styling

--- src/components/common/NavLinks.js
+++ src/components/common/NavLinks.js
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom'
 
 export default function NavLinks({ children, to }){
   return(
-    <NavLink to={to} className={({isActive}) => isActive ? 'text-primary-purple font-semibold link-shadow' : 'font-medium text-primary-dark link-shadow hover:text-primary-purple'}>
+    <NavLink to={to} className={({isActive}) => isActive ? 'text-primary-purple font-semibold link-shadow first-letter:uppercase' : 'font-medium text-primary-dark link-shadow hover:text-primary-purple first-letter:uppercase'}>
       {children}
     </NavLink>
   );
